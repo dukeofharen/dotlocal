db07d4a46f73833be61ff76184be3c23f23ab2ff
Duco Winterwerp <duco@winterwerp.it>
2016-04-10 12:45:00 +0200

    Updated AfterBuild.ps1

--- AfterBuild.ps1
+++ AfterBuild.ps1
@@ -1,2 +1,6 @@
 $configuration = (Get-Childitem env:CONFIGURATION).Value
 Rename-Item "Ducode.Local\bin\$configuration\Ducode.Local.exe" dotlocal.exe
+Rename-Item "Ducode.Local\bin\$configuration\Ducode.Local.exe.config" App.config
+
+Get-Childitem . -recurse -include *.xml -force | Remove-Item
+Get-Childitem . -recurse -include *.pdb -force | Remove-Item
