02bca001e497ed3a3284c43e82b3d6d06c2d91aa
Duco Winterwerp <duco@winterwerp.it>
2016-04-10 12:26:02 +0200

    Updated AfterBuild.ps1

--- AfterBuild.ps1
+++ AfterBuild.ps1
@@ -1,2 +1,2 @@
-$configuration = (Get-Childitem env:path).Value
-Rename-Item "Ducode.Local\bin\$configuration\Ducode.Local.exe" dotlocal.exe
+$configuration = (Get-Childitem env:CONFIGURATION).Value
+//Rename-Item "Ducode.Local\bin\$configuration\Ducode.Local.exe" dotlocal.exe
